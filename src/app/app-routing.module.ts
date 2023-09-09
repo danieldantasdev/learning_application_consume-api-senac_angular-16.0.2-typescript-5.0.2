68ebe0503e36cf5aabc8d9187c6504ccc85084b7
danieldantasdev <contatolobinhodev@gmail.com>
2023-09-09 15:21:55 -0300

    feat: add routing for component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./features/pages/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { title:"Home", path:"home", component: HomeComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: '**', pathMatch: 'full', redirectTo: '/home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
